0053c2243f15183164a38e7d74cf4004140c4f69
satof3 <satof.design@gmail.com>
2023-03-09 15:25:52 +0900

    リンク先を修正

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -25,7 +25,7 @@
         </div><!-- / header content -->
       </div><!-- / container -->
     </header><!-- / header -->
-    <a href="https://growi.org/plugins">
+    <a href="https://connpass.com/event/277641/">
     <img src="/assets/images/startup-seminar-banner.png" alt="Seminar Banner" style="margin-top: 70px;"/>
   </a>
     <div v-if="data.features && data.features.length" id="features" data-section="features">
